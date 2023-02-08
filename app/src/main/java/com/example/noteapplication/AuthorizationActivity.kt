6ef378298c181ee0af2506c7636e56306f51351e
Vitaly Gukov <vgukov@outlook.com>
2023-02-08 22:11:50 +0300

    Change EditText to TextInputLayout. Add verification to AuthorizationActivity and RegistrationActivity.

--- app/src/main/java/com/example/noteapplication/AuthorizationActivity.kt
+++ app/src/main/java/com/example/noteapplication/AuthorizationActivity.kt
@@ -4,20 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.textfield.TextInputLayout
 
 class AuthorizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
 
+        val til_login = findViewById<TextInputLayout>(R.id.til_email)
+        val et_login: EditText? = til_login.editText
+        et_login?.doAfterTextChanged {
+            til_login.error = null
+        }
+
+        val til_pass = findViewById<TextInputLayout>(R.id.til_password)
+        val et_pass: EditText? = til_pass.editText
+        et_pass?.doAfterTextChanged {
+            til_pass.error = null
+        }
+
         findViewById<Button>(R.id.btn_login).setOnClickListener {
-            Toast.makeText(this, "Нажал кнопку Log-in", Toast.LENGTH_SHORT).show()
+            if (et_login?.text.toString().isEmpty()) {
+                til_login.error = getString(R.string.obligatory_field)
+            }
+            if (et_pass?.text.toString().isEmpty()) {
+                til_pass.error = getString(R.string.obligatory_field)
+            } else {
+                Toast.makeText(applicationContext, "Все ок!!!! ", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
         findViewById<TextView>(R.id.tv_sign_up).setOnClickListener {
             startActivity(Intent(this, RegistrationActivity::class.java))
         }
+
+
     }
 }
--- app/src/main/java/com/example/noteapplication/RegistrationActivity.kt
+++ app/src/main/java/com/example/noteapplication/RegistrationActivity.kt
@@ -4,13 +4,57 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.textfield.TextInputLayout
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val til_name = findViewById<TextInputLayout>(R.id.til_name)
+        val et_name:EditText? = til_name.editText
+        et_name?.doAfterTextChanged {
+            if (!isValidName(et_name.text.toString().trim())){
+                til_name.error = getString(R.string.error_name)
+            }else{
+                til_name.error = null
+            }
+        }
+
+        val til_surnamename = findViewById<TextInputLayout>(R.id.til_surname)
+        val et_surnamename:EditText? = til_surnamename.editText
+        et_surnamename?.doAfterTextChanged {
+            if (!isValidName(et_surnamename.text.toString().trim())){
+                til_surnamename.error = getString(R.string.error_name)
+            }else{
+                til_surnamename.error = null
+            }
+        }
+
+        val til_email = findViewById<TextInputLayout>(R.id.til_email)
+        val et_email:EditText? = til_email.editText
+        et_email?.doAfterTextChanged {
+            if (!isValidEmail(et_email.text.toString().trim())){
+                til_email.error = getString(R.string.error_name)
+            }else{
+                til_email.error = null
+            }
+        }
+
+        val til_password = findViewById<TextInputLayout>(R.id.til_password)
+        val et_password:EditText? = til_password.editText
+        et_password?.doAfterTextChanged {
+            if (!isValidPassword(et_password.text.toString().trim())){
+                til_password.error = getString(R.string.error_name)
+            }else{
+                til_password.error = null
+            }
+        }
+
 
         findViewById<Button>(R.id.btn_sign_up).setOnClickListener {
             Toast.makeText(this, "Нажал кнопку Sign-up", Toast.LENGTH_SHORT).show()
@@ -19,5 +63,15 @@ class RegistrationActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.tv_login).setOnClickListener {
             startActivity(Intent(this, AuthorizationActivity::class.java))
         }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}".toRegex())
+    }
+    private fun isValidName(name: String):Boolean{
+        return name.matches("[a-zA-Zа-яёА-ЯЁ]{3,255}".toRegex())
+    }
+    private fun isValidPassword(password:String):Boolean{
+        return password.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,50}\$".toRegex())
     }
 }
